23ed064da9fa7c802ab472a3e787caadd677cde0
Daniel Catalan <dancatalan415@gmail.com>
2022-10-20 17:50:45 -0700

    gpio init

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,31 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 use cortex_m::asm;
 use cortex_m_rt::entry;
 
+
+fn gpio_init()
+{
+
+    // set PA5 to output, push/pull, pull-up
+
+    // PortA is at 0x40020000
+    let port_addr = 0x4002_0000;
+    let pin = 0x20;
+
+    let bsrr_arr = port_addr + 0x18;
+    let bsrr_value = pin << 16;
+
+    // Reset pin
+    unsafe{
+        core::ptr::write_volatile(bsrr_arr as *mut u32, bsrr_value)
+    }
+
+}
+
 #[entry]
 fn main() -> ! {
     asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+
+    gpio_init();
 
     loop {
         // your code goes here
