4db59547de3578e8af740c68cb8c07a029b74dff
Daniel Catalan <dancatalan415@gmail.com>
2022-11-15 11:46:08 -0800

    Flash relase build

--- flash.bat
+++ flash.bat
@@ -1,1 +1,1 @@
-openocd -f interface/stlink.cfg -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase target/thumbv7em-none-eabihf/debug/blink" -c "reset run" -c exit
+openocd -f interface/stlink.cfg -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase target/thumbv7em-none-eabihf/release/blink" -c "reset run" -c exit
