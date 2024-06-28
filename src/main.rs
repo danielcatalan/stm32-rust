0030d7093f209493c3733cb864c21bedad245d6d
Daniel Catalan <dancatalan415@gmail.com>
2024-06-28 15:45:27 -0700

    using new hal crate

--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,34 @@
 
 // pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-                     // use panic_abort as _; // requires nightly
-                     // use panic_itm as _; // logs messages over ITM; requires ITM support
-                     // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
-
+// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 use cortex_m_rt::entry;
-use cortex_m::delay::Delay;
-
-use stm32_hal2::{
-    clocks::Clocks,
-    gpio::{Pin, Port, PinMode, OutputType},
-    pac
-};
-
+use stm32f4xx_hal::{gpio::PinState, pac, prelude::*, rcc::RccExt};
 
 #[entry]
 fn main() -> ! {
-    let _dp = pac::Peripherals::take().unwrap();
-
-    let clock_cfg = Clocks::default();
-    clock_cfg.setup().unwrap();
-
+    let dp = pac::Peripherals::take().unwrap();
     let cp = cortex_m::Peripherals::take().unwrap();
-    let mut delay = Delay::new(cp.SYST, clock_cfg.systick());
 
-    let mut pin5 = Pin::new(Port::A, 5, PinMode::Output);
-    pin5.output_type(OutputType::PushPull);
-    pin5.set_low();
-    pin5.output_speed(stm32_hal2::gpio::OutputSpeed::High);
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.sysclk(180.MHz()).freeze();
+
+
+    let mut delay = cp.SYST.delay(&clocks);
+
+    let gpioa = dp.GPIOA.split();
+    let mut led = gpioa.pa5.into_push_pull_output();
 
     let mut set_on: bool = false;
 
-    const DELAY_TIME_MS: u32  = 100;
+    const DELAY_TIME_MS: u32 = 1000;
 
     loop {
         match set_on {
-            false => pin5.set_low(),
-            true => pin5.set_high()
+            true => led.set_state(PinState::High),
+            false => led.set_state(PinState::Low),
         }
+
         delay.delay_ms(DELAY_TIME_MS);
         set_on = !set_on;
     }
